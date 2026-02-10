099c540a2e3333c0ad847921c84df83f295d09de
Zyqrin <wisnuwisnu819863@gmail.com>
2026-02-11 00:33:50 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
 local games = {
-    [138048509924404] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/Zyqrin/Save-Brainrots-from-LAVA/refs/heads/main/Main%20Lua",
+    [138048509924404] = "https://raw.githubusercontent.com/Zyqrin/Save-Brainrots-from-LAVA/refs/heads/main/Main%20Lua",
 }
 
 local scriptURL = games[game.PlaceId]
 
 if scriptURL then
-    loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/Zyqrin/Save-Brainrots-from-LAVA/refs/heads/main/Main%20Lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zyqrin/Save-Brainrots-from-LAVA/refs/heads/main/Main%20Lua"))()
 else
     game.Players.LocalPlayer:Kick("Game ini tidak didukung")
 end
